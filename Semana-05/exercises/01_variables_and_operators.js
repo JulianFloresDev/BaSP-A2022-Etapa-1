4666c68541087f68a014ad2b9b74c990d4f16295
Julian Flores <julianfloresdev@gmail.com>
2022-09-15 16:49:36 -0300

    BaSP-W05-2022: Finish first exercise / Index all js files in HTML

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+/**************************
+**    Variables & Operators
+***************************/
+//? a)
+var fisrtValue = 7987124;
+var secondValue = 166242;
+
+var thirdValue = fisrtValue + secondValue;
+console.log('The first value add second value is: ',thirdValue);
+
+//? b)
+var firstString = 'Que rica es la';
+var secondString = '... droga, obvio!';
+
+var concatString = firstString + secondString;
+console.log(concatString);
+
+//? c)
+var stringLength1 = 'Mentira, no me drogo...';
+var stringLength2 = 'Fumar porro no es drogarse!';
+
+var finalLength = stringLength1.length + stringLength2.length;
+console.log(finalLength);
