55d7d54163aa827d5f75eb2bf88206bb51a60ca3
Julian Flores <julianfloresdev@gmail.com>
2022-09-19 21:19:16 -0300

    BaSP-W05-2022: excersice 6 line 21 simplify return

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -13,29 +13,26 @@ function sumValid(num1, num2) {
     if ((typeof num1 !== "number") || (typeof num2 !== "number")) {
         alert('Exercise six.\nItem b)\n\nSome param is not a number. Check It Bro!!');
         return NaN
-    }
+    } return suma(num1,num2);
 }
 
 //? c)
 function validateInteger(num) {
-    if (Number.isInteger(num)) {
-        return true
-    } else return false
+    return Number.isInteger(num);
 }
 
 //? d)
 function sumValid2(num1, num2) {
     if (!validateInteger(num1)) {
         alert('Exercise six.\nItem d)\n\nThe first number is nor integer\n\n' + num1);
-        return Math.round(num1 + num2);
+        return Math.round(sumValid(num1,num2));
     } else if (!validateInteger(num2)) {
         alert('Exercise six.\nItem d)\n\nThe second number is not integer\n\n' + num2);
-        return Math.round(num1 + num2);
-    } else return num1 + num2;
+        return Math.round(sumValid(num1,num2));
+    } else return sumValid(num1,num2);
 }
 
 //? e)
 function sumComplete(num1,num2){
-    sumValid(num1,num2);
     return sumValid2(num1,num2);
 }
