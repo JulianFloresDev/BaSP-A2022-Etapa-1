d40b142b46a29732879708e16fb7105412a50a4e
Julian Flores <julianfloresdev@gmail.com>
2022-09-24 21:25:46 -0300

    BaSP-W06-2022: Create / Design / Edit Sing Up Page

--- Semana-06/js/login.js
+++ Semana-06/js/login.js
@@ -8,7 +8,7 @@ window.onload = function () {
         required: 'This is a required field *',
         emailMin: 'This field may contain from 3 characters *',
         emailValid: 'This field must contain a valid email *',
-        passwordValid: 'This field can contain from 4 to 25 characters *',
+        passwordValid: 'This field can contain from 8 to 25 characters *',
         passwordSpaces: 'This field cannot contain spaces *'
     };
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
@@ -60,7 +60,7 @@ window.onload = function () {
     inputPassword.onblur = function () {
         if (this.value.length == 0) {
             inputInvalid(this, this.nextElementSibling, alertErrorText.required);
-        } else if (this.value.length < 4 || this.value.length > 25) {
+        } else if (this.value.length < 8 || this.value.length > 25) {
             inputInvalid(this, this.nextElementSibling, alertErrorText.passwordValid);
         } else {
             if (this.value.indexOf(' ') == -1) {
